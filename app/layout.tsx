81afbd389d5113714ee4c5a5849d7a66ba501a02
ANKIT <kumarankit0327@gmail.com>
2024-06-08 01:37:03 +0530

    clerk setup for user management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import ConvexClientProvider from "./providers/ConvexProvider";
+import ConvexClerkProvider from "./providers/ConvexClerkProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><ConvexClientProvider>{children}</ConvexClientProvider></body>
+      <body className={inter.className}><ConvexClerkProvider>{children}</ConvexClerkProvider></body>
     </html>
   );
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,13 @@
+
+const authConfig = {
+    providers: [
+        {
+            domain: "https://big-gazelle-54.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ]
+};
+
+
+export default authConfig;
+
