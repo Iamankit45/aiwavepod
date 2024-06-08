a9a1ec1da106bdbe98788105206474c3ea152baa
ANKIT <kumarankit0327@gmail.com>
2024-06-08 20:32:07 +0530

    added OpenAI for generating podcast

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,7 @@ import LeftSideBar from "@/components/LeftSideBar";
 import MobileNav from "@/components/MobileNav";
 import RightSideBar from "@/components/RightSideBar";
 import Image from "next/image";
+import { Toaster } from "@/components/ui/toaster"
 
 export default function RootLayout({
   children,
@@ -24,7 +25,7 @@ export default function RootLayout({
              <MobileNav/>
             </div>
             <div className="flex flex-col md:pb-14">
-              Toaster
+            <Toaster />
 
               {children}
             </div>
---
+++ convex/podcasts.ts
@@ -0,0 +1,15 @@
+import { ConvexError, v } from "convex/values";
+
+import { mutation, query } from "./_generated/server";
+
+
+// this mutation is required to generate the url after uploading the file to the storage.
+export const getUrl = mutation({
+  args: {
+    storageId: v.id("_storage"),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.storage.getUrl(args.storageId);
+  },
+});
+
